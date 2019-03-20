5f11de096aaf4e4a0dda5940b035ca2aad7a4310
AVANADE-CORP\michal.krupej <michal.krupej@avanade.com>
2019-03-20 10:07:16 +0100

    Revert "cleaning old project files"
    This reverts commit ca8200478c666dab0b93c727f55cb84be78ca43d.

---
+++ SpotifyAutomation/SpotifyWebPlayerAndApiTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SpotifyWebPlayerAndApiTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SpotifyWebPlayerAndApiTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("303d0145-0871-4376-b16f-88609fd6cf4e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
